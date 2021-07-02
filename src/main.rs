72c008171645a574ad8192d997110d67a27dcfb3
motokishibata <motok.shibat@gmail.com>
2021-07-02 17:57:12 +0900

    1つの数値を出力

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("invalid arguments");
+        return;
+    }
+
+    print!(".intel_syntax noprefix\n");
+    print!(".globl main\n");
+    print!("main:\n");
+    print!("  mov rax, {}\n", args[1]);
+    print!("  ret\n");
 }
