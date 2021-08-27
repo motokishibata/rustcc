abcadb7a122761e700e2cbf3bcbe8e1ec3c8af54
motokishibata <motok.shibat@gmail.com>
2021-08-27 13:29:14 +0900

    出力用オプション、ファイルからの読み込み追加

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rustcc::*;
 use std::env;
 use std::io::Write;
 use std::fs::File;
+use std::io::prelude::*;
 
 mod token;
 mod parse;
@@ -15,22 +16,55 @@ use gen::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() < 2 {
         eprintln!("invalid arguments");
         return;
     }
-    let mut input = args[1].as_str();
-    // cargo runでは "-" 始まりの引数を渡せないためエスケープ用の文字を追加
-    let first = &input[0..1];
-    if first == "^" {
-        input = &input[1..];
+
+    let mut is_print_contents = false;
+    let mut is_print_asm = false;
+    let mut is_file = false;
+    let mut input = String::new();
+    for arg in args[1..].iter() {
+        // cargo runでは "-" 始まりの引数を渡せないためエスケープ用の文字を追加
+        let mut arg: &str = arg;
+        if &arg[0..1] == "^" {
+            arg = &arg[1..];
+        }
+        match arg {
+            "-i" => is_print_contents = true,
+            "-a" => is_print_asm = true,
+            "-f" => is_file = true,
+            _ => input = arg.to_string()
+        }
     }
 
-    let tokens = tokenize(input);
+    compile(input, is_file, is_print_contents, is_print_asm);
+}
+
+fn compile(input: String, is_file: bool, is_print_contents: bool, is_print_asm: bool) {
+    let mut contents = String::new();
+    if is_file {
+        let mut f = File::open(input).expect("file not found");
+        f.read_to_string(&mut contents).expect("failure read file");
+    } else {
+        contents = input;
+    }
+
+    let tokens = tokenize(contents.as_str());
     let nodes = parse(&tokens);
     let asm = gen_x86(nodes);
 
-    println!("{}", asm);
+    if is_print_contents {
+        println!("------- read contents -------");
+        println!("{}", contents);
+        println!("-----------------------------\n");
+    }
+    if is_print_asm {
+        println!("------- output assembly -------");
+        println!("{}", asm);
+        println!("-------------------------------\n");
+    }
 
     if let Ok(v) = File::create("./tmp.s") {
         let mut file = v;
